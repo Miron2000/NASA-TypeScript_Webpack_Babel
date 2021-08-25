3765c672839bf787a2d3bead667b97f7b1158520
miron <miron.ivasch@gmail.com>
2021-08-25 12:31:05 +0300

    Refactoring test

--- src/components/MainSection/__test__/MainSection.test.tsx
+++ src/components/MainSection/__test__/MainSection.test.tsx
@@ -55,29 +55,29 @@ test('Simulates selection component SelectMarsRover', () => {
   expect((screen.getByText('Spirit') as HTMLOptionElement).selected).toBeFalsy();
 });
 
-global.fetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve({
-    data: {
-      id: 102693,
-      sol: 1000,
-      camera: {
-        id: 20,
-        name: 'FHA',
-        rover_id: 5,
-        full_name: 'Front Hazard Avoidance Camera',
-      },
-      img_src: 'https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG',
-      earth_date: '2015-05-30',
-      rover: {
-        id: 5,
-        name: 'Curiosity',
-        landing_date: '2012-08-06',
-        launch_date: '2011-11-26',
-        status: 'active',
-      },
-    },
-  }),
-}));
+// global.fetch = jest.fn(() => Promise.resolve({
+//   json: () => Promise.resolve({
+//     data: {
+//       id: 102693,
+//       sol: 1000,
+//       camera: {
+//         id: 20,
+//         name: 'FHA',
+//         rover_id: 5,
+//         full_name: 'Front Hazard Avoidance Camera',
+//       },
+//       img_src: 'https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG',
+//       earth_date: '2015-05-30',
+//       rover: {
+//         id: 5,
+//         name: 'Curiosity',
+//         landing_date: '2012-08-06',
+//         launch_date: '2011-11-26',
+//         status: 'active',
+//       },
+//     },
+//   }),
+// }));
 
 // Тест на проверку Запроса АПИ (fetch)
 test('Fetches and displays data', async () => {
